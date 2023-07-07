8ee7f762c80d8b800ebe52a723b7b1f1322244b8
Katherine S Nelson <ksnelson@ksu.edu>
2023-07-07 11:43:27 -0500

    Initial script upload.

---
+++ Run pca script.R
@@ -0,0 +1,54 @@
+library(sf)
+
+ct <- st_read("E:/Intersection of Social Vulnerability and Flood/tlgdb_2018_a_us_substategeo.gdb", layer = "Census_Tract")
+ct$STATEFP <- substr(ct$GEOID,1,2) 
+ct$COUNTYFP <- substr(ct$GEOID,1,5) 
+st_write(ct,"tl_2018_censustract.shp", driver = "ESRI Shapefile", append=FALSE)
+
+bg <- st_read("E:/Intersection of Social Vulnerability and Flood/tlgdb_2018_a_us_substategeo.gdb", layer = "Block_Group")
+bg$STATEFP <- substr(bg$GEOID,1,2) 
+bg$COUNTYFP <- substr(bg$GEOID,1,5) 
+st_write(bg,"tl_2018_blockgroup.shp", driver = "ESRI Shapefile", append=FALSE)
+
+
+
+wd<-getwd()
+wd<-"E:/Intersection of Social Vulnerability and Flood/Run scripts"
+source(paste0(wd,'/pca_kn_new.R'))
+
+
+#Run county data
+s<-build.pca('/county data by states/all_Final_Table.csv', '/tl_2018_us_county.shp', 'US', 'county') 
+s<-build.pca('/county data by states/all_Final_Table.csv', '/tl_2018_us_county.shp', 'State', 'county') 
+
+
+#Combine and run pca on census tract data
+files<-list.files(path= paste0(wd,"/census tract data by states/"), pattern= '_Final_Table') #get list of intersection files
+f<-paste0(wd,"/census tract data by states/",files, sep="")
+
+states<-lapply(f, read.csv) #read in all datafiles 
+all_states<-do.call(rbind, states) #merge all states
+library(readr)
+write_csv(all_states, paste0(wd,"/census tract data by states/all_states.csv"))
+
+s<-build.pca('/census tract data by states/all_states.csv', '/tl_2018_censustract.shp', 'US', 'tract') 
+s<-build.pca('/census tract data by states/all_states.csv', '/tl_2018_censustract.shp', 'State', 'tract') 
+s<-build.pca(data='/census tract data by states/all_states.csv', boundaries='/tl_2018_censustract.shp', extent='County', scale='tract') 
+
+
+#Combine and run pca on block group data
+files<-list.files(path= paste0(wd,"/block group data by states/"), pattern= '_Final_Table') #get list of intersection files
+f<-paste0(wd,"/block group data by states/",files, sep="")
+
+states<-lapply(f, read.csv) #read in all datafiles 
+all_states<-do.call(rbind, states) #merge all states
+library(readr)
+write_csv(all_states, paste0(wd,"/block group data by states/all_states.csv"))
+
+s<-build.pca(data='/block group data by states/all_states.csv', boundaries='/tl_2018_blockgroup.shp', extent='US', scale='blockgroup') 
+s<-build.pca('/block group data by states/all_states.csv', '/tl_2018_blockgroup.shp', 'State', 'blockgroup') 
+s<-build.pca(data='/block group data by states/all_states.csv', boundaries='/tl_2018_blockgroup.shp', extent='County', scale='blockgroup2') 
+
+
+t<-readRDS(paste0(wd,"/SVI_County_blockgroup2.rds"))
+t<-readRDS(paste0(wd,"/SVI_State_blockgroup.rds"))
